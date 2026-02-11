b015be7133c534a6d7c6a802e623a0bd2a4acf6d
aayush <aayushs290107@gmail.com>
2026-02-11 20:22:30 +0530

    HomePage Background component

---
+++ raise-in/components/ui/beams-background.jsx
@@ -0,0 +1,8 @@
+"use client";
+import { BeamsBackground } from "@/app/components/beams-background";
+
+export { BeamsBackground };
+
+export function BeamsBackgroundDemo() {
+  return <BeamsBackground />;
+}
